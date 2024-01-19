83f46f28a9d567dbb3d112e9bb40d7e6eb9fe849
JoasMartins <joasmcarmo@gmail.com>
2024-01-18 23:29:51 -0300

    Bug Commit 2

--- src/app/agendaaprendize/contratar/page.jsx
+++ src/app/agendaaprendize/contratar/page.jsx
@@ -27,19 +27,6 @@ export default function PageContratar() {
 
     function handleCTTwhatsapp() {
         window.open("https://api.whatsapp.com/send?phone=5516988318960&text=Ol%C3%A1%2C%20eu%20me%20interessei%20pelo%20projeto%20Agenda%20Aprendize%20e%20gostaria%20de%20contrata-lo%20para%20ser%20usado%20em%20minha%20escola.")
-        window.open("mailto:applicco.equipe@gmail.com")
-    }
-
-    function handleMoreServiceAPC() {
-        window.open("mailto:applicco.equipe@gmail.com?subject=Solicitação de Serviço&body=Olá, eu gostaria de saber mais sobre os serviços em que a Applicco desenvolve e desejo solicitar um deles.")
-    }
-
-    function handleCTTemail() {
-        window.open("mailto:applicco.equipe@gmail.com?subject=CONTRATO AGENDA APRENDIZE")
-    }
-
-    function handleCTTwhatsapp() {
-        window.open("https://api.whatsapp.com/send?phone=5516988318960&text=Ol%C3%A1%2C%20eu%20gostaria%20de%20contratar%20o%20Agenda%20Aprendize.")
     }
 
     function handleCTTinstagram() {
@@ -57,101 +44,91 @@ export default function PageContratar() {
                 </div>
 
                 <div className="textsInfo">
-                    <div className="texts">
-                        <div>
-                            <h3>Oque é contratar o Agenda Aprendize?</h3>
-                            <p>Sua escola terá acesso ao aplicativo e ele será totalmente personalizado para o seu colégio. Cada escola que contrata o nosso projeto, terá um aplicativo exclusivo com a sua identidade visual e com os recursos padrão que o aplicativo oferece.</p>
-                        </div>
-                        <div>
-                            <Image src="/phone.svg" width={180} height={180} />
-                        </div>
+                    <div>
+                        <h3>Oque é contratar o Agenda Aprendize?</h3>
+                        <p>Sua escola terá acesso ao aplicativo e ele será totalmente personalizado para o seu colégio. Cada escola que contrata o nosso projeto, terá um aplicativo exclusivo com a sua identidade visual e com os recursos padrão que o aplicativo oferece.</p>
                     </div>
-
-                    <div className="contratar">
-                        <h3>Iniciar Contrato</h3>
-                        <p className="valor">O valor é conversado e definido ao entrar em contato conosco.<br />Para contratar nos mande um email, mensagem no whatsapp ou no instagram.</p>
-                        <h4>Para contratar, <span>nos mande uma mensagem</span> através de um dos nossos meios de contato abaixo.</h4>
+                    <div>
+                        <Image src="/phone.svg" width={180} height={180} />
                     </div>
+                </div>
 
-                    <div className="contact">
+                <div className="contratar">
+                    <h3>Iniciar Contrato</h3>
+                    <p className="valor">O valor é conversado e definido ao entrar em contato conosco.<br />Para contratar nos mande um email, mensagem no whatsapp ou no instagram.</p>
+                    <h4>Para contratar, <span>nos mande uma mensagem</span> através de um dos nossos meios de contato abaixo.</h4>
+                </div>
 
-                    </div>
+                <div className="contact">
 
-                    <div className="contact">
-                        <h3>Meios de Contato:</h3>
+                    <h3>Meios de Contato:</h3>
 
-                        <div className="actions">
+                    <div className="actions">
 
-                            <div className="buttonArea">
-                                <button onClick={handleCTTemail}>
-                                    <Image src={"/email-icon.png"} width={20} height={20} className="icon" id="email" />
-                                    <Image />
-                                    <div className="divisoria" />
-                                    <p>Via Email</p>
-                                </button>
-                                <div>
-                                    <p>contato@applicco.com.br</p>
-                                </div>
-                            </div>
-
-                            <div className="buttonArea">
-                                <button onClick={handleCTTwhatsapp}>
-                                    <Image src={"/whatsapp-logo.png"} width={20} height={20} className="icon" />
-                                    <Image />
-                                    <div className="divisoria" />
-                                    <p>Via WhatsApp</p>
-                                </button>
-                                <div>
-                                    <p>+55 (16) 98831-8960</p>
-                                    <p>(Número pessoal)</p>
-                                </div>
-                            </div>
-
-                            <div className="buttonArea">
-                                <button onClick={handleCTTinstagram}>
-                                    <Image src={"/instagram-logo.png"} width={20} height={20} className="icon" />
-                                    <Image />
-                                    <div className="divisoria" />
-                                    <p>Via Instagram</p>
-                                </button>
-                                <div>
-                                    <p>@appliccoapps</p>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div className="dev">
-                        <p className="title">Esse é um projeto desenvolvido por:</p>
-                        <img src="/appliccoBanner.png" className="appliccoLogo" />
-                        <div className="buttons">
-                            <button onClick={handleInstagramAPC}>
-                                <img src="/instagram-logo.png" />
-                                <p>Instagram</p>
+                        <div className="buttonArea">
+                            <button onClick={handleCTTemail}>
+                                <Image src={"/email-icon.png"} width={20} height={20} className="icon" id="email" />
+                                <div className="divisoria" />
+                                <p>Via Email</p>
                             </button>
+                            <div>
+                                <p>contato@applicco.com.br</p>
+                            </div>
+                        </div>
 
-                            {/*
-                            <img src="/instagramLogo.png" />
+                        <div className="buttonArea">
+                            <button onClick={handleCTTwhatsapp}>
+                                <Image src={"/whatsapp-logo.png"} width={20} height={20} className="icon" />
+                                <div className="divisoria" />
+                                <p>Via WhatsApp</p>
+                            </button>
+                            <div>
+                                <p>+55 (16) 98831-8960</p>
+                                <p>(Número pessoal)</p>
+                            </div>
+                        </div>
+
+                        <div className="buttonArea">
+                            <button onClick={handleCTTinstagram}>
+                                <Image src={"/instagram-logo.png"} width={20} height={20} className="icon" />
+                                <div className="divisoria" />
+                                <p>Via Instagram</p>
+                            </button>
+                            <div>
+                                <p>@appliccoapps</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="dev">
+                    <p className="title">Esse é um projeto desenvolvido por:</p>
+                    <img src="/appliccoBanner.png" className="appliccoLogo" />
+                    <div className="buttons">
+                        <button onClick={handleInstagramAPC}>
+                            <img src="/instagram-logo.png" />
                             <p>Instagram</p>
                         </button>
+                        
+                        {/*
                         <button onClick={handleContactAPC}>
                             <FontAwesomeIcon icon={faEnvelope} className="icon" />
                             <p>Entrar em contato</p>
                         </button>
                         */}
-                            <button onClick={handleMoreServiceAPC}>
-                                <FontAwesomeIcon icon={faPlus} className="icon" />
-                                <p>Solicitar outro serviço</p>
-                            </button>
-                        </div>
-                        <div className="texts">
-                            <p>A Applicco é gerenciada por <a href="https://www.instagram.com/joas_mc_" target="__blank">Joás Martins</a>,</p>
-                            <p>aluno do 3º ano em <a>Juiz de Fora</a>.</p>
-                        </div>
+                        <button onClick={handleMoreServiceAPC}>
+                            <FontAwesomeIcon icon={faPlus} className="icon" />
+                            <p>Solicitar outro serviço</p>
+                        </button>
+                    </div>
+                    <div className="texts">
+                        <p>A Applicco é gerenciada por <a href="https://www.instagram.com/joas_mc_" target="__blank">Joás Martins</a>,</p>
+                        <p>aluno do 3º ano em <a>Juiz de Fora</a>.</p>
                     </div>
                 </div>
             </div>
+
 
         </main>
     )
--- src/app/agendaaprendize/page.jsx
+++ src/app/agendaaprendize/page.jsx
@@ -5,12 +5,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload, faHandshake } from '@fortawesome/free-solid-svg-icons';
 import appData from "../../appData.json"
 import "../css/Agendaaprendize.css"
-
 import Image from 'next/image';
 
 import SessionHome from "../../components/SessionHome";
 
-import SessionHome from "../../components/SessionHome";
+
 
 function Index() {
 
@@ -45,7 +44,6 @@ function Index() {
                             <div className='texts'>
                                 <h3>Contratar o aplicativo para a sua escola:</h3>
                                 <p>Ao contratar o Agenda Aprendize para a sua escola, o nome do app será mudado para o do seu colégio, assim como sua foto, suas cores e tudo oque está associado a identidade visual, fazendo com que o seu app seja 100% desvínculado a E. E. Sebastião Patrus de Souza.</p>
-                                <p>Ao contratar o Agenda Patrus para a sua escola, o nome do app será mudado para o do seu colégio, assim como sua foto, suas cores e tudo oque está associado a identidade visual, fazendo com que o seu app seja 100% desvínculado a E. E. Sebastião Patrus de Souza.</p>
                                 <p>Aperte o botão ao lado para contratar o aplicativo.</p>
                             </div>
                             <button onClick={handleHire} className='areaActionButton'>
@@ -77,61 +75,106 @@ function Index() {
                 </div>
 
                 <SessionHome
-                title="APROVADO POR DIRETORES E SECRETÁRIOS"
-                text="Esse projeto foi aplicado na rede escolar E. E. Sebastião Patrus de Souza e obteve a aprovação e elogios da equipe administrativa, secretários e diretores, sem contar com a excelente avaliação de professores e alunos."
-                image="/aprovado.png"
-                color="#5e86ff"
-            />
+                    title="APROVADO POR DIRETORES E SECRETÁRIOS"
+                    text="Esse projeto foi aplicado na rede escolar E. E. Sebastião Patrus de Souza e obteve a aprovação e elogios da equipe administrativa, secretários e diretores, sem contar com a excelente avaliação de professores e alunos."
+                    image="/agendaaprendize/approved.svg"
+                    color="#5e86ff"
+                />
 
-    
+                <SessionHome
+                    title="O QUE É O PROJETO?"
+                    text="O Agenda Aprendize é um aplicativo móvel projetado para integrar diversas funcionalidades, visando simplificar a gestão escolar e potencializar a experiência de estudos. Com diversos recursos, o aplicativo oferece soluções tanto para alunos, quanto para professores e direção, promovendo eficiência e organização na rotina escolar."
+                    image="/agendaaprendize/project.svg"
+                    color="#292929"
+                    direction="right"
+                />
 
-            <SessionHome
-                title="O QUE É O PROJETO?"
-                text="O projeto Agenda Patrus é um aplicativo para celulares criado para reunir várias funções que agregam nos estudos e na gestão da escola."
-                image="/logo192.png"
-                color="#292929"
-                direction="right"
-            />
+                <SessionHome
+                    title="ÁREA DO PROFESSOR"
+                    text=""
+                    image="/agendaaprendize/owl-teacher.svg"
+                    color="#5e86ff"
+                    custom={4}
+                />
 
-            <SessionHome
-                title="SOBRE O APLICATIVO"
-                text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim error, aliquid quaerat quis alias amet dignissimos ad expedita optio iste. Eligendi dolore totam blanditiis sapiente saepe eum consequatur recusandae ab."
-                image="/logo192.png"
-                color="#5e86ff"
-            />
+                <SessionHome
+                    title="NOTIFICAÇÕES"
+                    text=''
+                    image="/agendaaprendize/phone.svg"
+                    color="#0c1127"
+                    direction="right"
+                    custom={3}
+                />
 
-            <SessionHome
-                title="TITULO TITULO"
-                text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim error, aliquid quaerat quis alias amet dignissimos ad expedita optio iste. Eligendi dolore totam blanditiis sapiente saepe eum consequatur recusandae ab."
-                image="/logo192.png"
-                color="#292929"
-                direction="right"
-            />
+                <SessionHome
+                    title="EXIBIÇÃO POR DIAS"
+                    text=''
+                    image="/agendaaprendize/calendar.svg"
+                    color="#292929"
+                    custom={1}
+                />
 
-                <div className='main_areaNotify'>
-                    <h3 className='main_titleNotify'>Sistema de Notificações</h3>
-                    <p className='main_textNotify'>Apelidado carinhosamente de MaiconNotify System, é o sistema de notificações da Agenda Patrus, criado para sempre avisar com antecedência sobre as tarefas programadas, para que você possa se preparar e não se esquecer delas. Confira abaixo como esse sistema funciona no aplicativo.</p>
-                    <p className='main_textNotify'>Sempre que o dia de uma tarefa estiver se aproximando, o sistema irá te avisar quantos dias restam para a data marcada. Por exemplo: Se a tarefa chamada &quot;Trabalho de Matemática&quot; estiver agendada para um determinado dia, quando faltar 3 dias para essa tarefa, e se a opção de avisar com 3 dias de antecedência estiver ativada em sua conta, o sistema irá te notificar sobre essa tarefa e outras que estejam próximas.</p>
-                    <p className='main_textNotify'>Vale ressaltar que o usuário terá a liberdade de escolher quantos dias antes deseja ser notificado, e essas configurações podem ser ajustadas conforme sua preferência.</p>
-                    <p className='main_'>Confira as opções:</p>
-                    <ul>
-                        <li>Quando uma tarefa for criada;</li>
-                        <li>Todos os dias as 04h listar as tarefas do dia;</li>
-                        <li>Tarefas que estão restando 1 dia;</li>
-                        <li>Tarefas que estão restando 2 dias;</li>
-                        <li>Tarefas que estão restando 3 dias;</li>
-                        <li>Tarefas que estão restando 4 dias;</li>
-                        <li>Tarefas que estão restando 5 dias;</li>
-                        <li>Tarefas que estão restando 6 dias;</li>
-                        <li>Tarefas que estão restando 7 dias;</li>
-                        <li>Tarefas que estão restando 10 dias.</li>
-                    </ul>
-                </div>
+                <SessionHome
+                    title="JORNAL DA ESCOLA"
+                    text=""
+                    image="/agendaaprendize/news.svg"
+                    color="#5e86ff"
+                    custom={5}
+                    textBadge={"Em Breve"}
+                />
+
+                <SessionHome
+                    title="PROPRIEDADES DAS TAREFAS"
+                    text=''
+                    image="/agendaaprendize/list.svg"
+                    color="#292929"
+                    direction="right"
+                    custom={2}
+                />
+
+                <SessionHome
+                    title="ESTATISTICAS"
+                    text="Acesso as informações que ficam armazenadas no app, como: a quantidade de tarefas atribuídas a cada aluno ou turma; as que foram feitas ou não por cada aluno; os estudantes que mais fizeram atividades; as turmas que mais realizam suas tarefas; turmas com mais tarefas atribuidas, entre outros dados."
+                    image="/agendaaprendize/statistics.svg"
+                    color="#5e86ff"
+                    textBadge={"Em Breve"}
+                />
+
+                <SessionHome
+                    title="MARCAR COMO FEITO"
+                    text="Cada aluno terá controle sobre suas tarefas, marcando quais já foram feitas ou não. Essas marcações são contabilizadas para estátistica sobre o aluno e separar oque já foi feito ou oque está pendente."
+                    image="/agendaaprendize/stamp.svg"
+                    color="#292929"
+                    direction="right"
+                />
+
+                <SessionHome
+                    title="SEPARAÇÃO POR TURMAS"
+                    text='Cada turma terá suas tarefas separadas das demais, projetado para exibir aos alunos de cada turma apenas as atividades que foram lançadas para a sua turma, ou seja, alunos de outras turmas terão em seus menus outras tarefas, aquelas que foram lançadas para sua turma.'
+                    image="/agendaaprendize/student.svg"
+                    color="#5e86ff"
+                />
+
+                <SessionHome
+                    title="USAR EM OUTRAS ESCOLAS"
+                    text=""
+                    image="/agendaaprendize/contract.svg"
+                    color="#292929"
+                    direction="right"
+                    custom={6}
+                />
+
+                <SessionHome
+                    title="DESENVOLVIMENTO DO APLICATIVO"
+                    text="Projeto começou a ser desenvolvido em Maio de 2023, por um aluno do segundo ano do ensino médio (na época) da rede escolar pública Estadual Sebatião Patrus de Souza. Esse mesmo desenvolvedor é fundador da Equipe Applicco, de criação de aplicativos, sites e softwares. Enfrentando problemas em seu cronograma de tarefas, iniciu o projeto que a príncipio serviria apenas para uso particular do mesmo, mas que foi decidido aumentar o alcance para beneficiar mais estudantes tanto de sua escola quanto de outras."
+                    image="/agendaaprendize/development.svg"
+                    color="#5e86ff"
+                />
 
                 <div className='main_areaInfo'>
                     <div>
-                        <a>!</a>
-                        <h3>Aviso</h3>
+                        <Image src={"/info.svg"} width={30} height={30} />
+                        <p>Aviso!</p>
                     </div>
                     <p className='destaque'>Este aplicativo não foi criado nem solicitado pela equipe da secretaria ou direção da escola!</p>
                     <p className='nao_destaque'>Portanto, quaisquer erros, falhas ou problemas no aplicativo não devem ser reportados a eles, mas sim ao desenvolvedor do aplicativo.</p>
